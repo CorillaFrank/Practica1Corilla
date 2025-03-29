137d69271ef637978f7658f62b9b3e1fe68c36e8
CorillaFrank <jhoncorilla2014@gmail.com>
2025-03-28 21:39:30 -0500

    Primer commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Practica1Corilla.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
