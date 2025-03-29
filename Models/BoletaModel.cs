33f29d4bf242b674cfa1d4b215a302a207f8b662
CorillaFrank <jhoncorilla2014@gmail.com>
2025-03-28 23:53:37 -0500

    prueba1corilla

--- Models/BoletaModel.cs
+++ Models/BoletaModel.cs
@@ -1,32 +1,16 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace Practica1Corilla.Models
 {
-    [Route("[controller]")]
-    public class BoletaModel : Controller
+    public class BoletaModel
     {
-        private readonly ILogger<BoletaModel> _logger;
-
-        public BoletaModel(ILogger<BoletaModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View("Error!");
-        }
+        public string Nombre { get; set; }
+        public string DNI { get; set; }
+        public decimal Monto { get; set; }
+        public string Moneda { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
     }
 }
